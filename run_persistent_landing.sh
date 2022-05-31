5363b9dfab3aa8bfdf451418082508d52f4205f2
herozero777 <ismailhero@gmail.com>
2022-06-01 00:22:57 +0200

    Switch to py37 for pySpark on p1 run_persistent_landing.sh

--- run_persistent_landing.sh
+++ run_persistent_landing.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-/home/bdm/anaconda3/bin/python /home/bdm/sense-stock/src/stock_raw_to_hdfs.py --prod |& tee -a /home/bdm/sense-stock/logs/stock_raw_to_hdfs.log
+/home/bdm/anaconda3/envs/py37/bin/python /home/bdm/sense-stock/src/stock_raw_to_hdfs.py --prod |& tee -a /home/bdm/sense-stock/logs/stock_raw_to_hdfs.log
